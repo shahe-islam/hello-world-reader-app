8b3bcd734dcde9a7e3f94b28f7c581509a24f8d9
Shahe Islam <shahe.islam@nationwide.co.uk>
2020-11-04 13:12:37 +0000

    Modded Python script to take into account future considerations

--- helloWorldReaderApp/hwreaderapp.py
+++ helloWorldReaderApp/hwreaderapp.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import os, time, sys, logging
+ 
+#Change this variable to reference a specific environment variable to create logging for
+ENV = 'HELLOWORLD'
+#References period of logging for ENV
+TIMER = 3
+ 
+def main():
+ logging.basicConfig(filename='helloWorldReadApp.log', level=logging.INFO)
+ 
+ if ENV not in os.environ:
+    logging.info('%s is not an environment variable', ENV)
+ else:
+    print('Success')
+    logging.info('Logging envrionment variable %s', os.environ[ENV])
+ 
+while True:
+ main()
+ time.sleep(TIMER)
+
+ ### Look into addHandler for logging to print out the log to the screen effectively

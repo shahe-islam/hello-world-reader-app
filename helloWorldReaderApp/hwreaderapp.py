effef96de30106e0ad615b20b273f3be355fe906
Shahe Islam <shahe.islam@nationwide.co.uk>
2020-11-04 18:37:07 +0000

    added sys argv to passin in variables at playbook level

--- helloWorldReaderApp/hwreaderapp.py
+++ helloWorldReaderApp/hwreaderapp.py
@@ -3,16 +3,17 @@
 import os, time, sys, logging
 
 #References period of logging for ENV
-TIMER = 3
+ENV = sys.argv[1]
+TIMER = sys.argv[2]
  
 def main():
  logging.basicConfig(filename='helloWorldReadApp.log', level=logging.INFO)
  
- if [1] not in os.environ:
-    logging.info('%s is not an environment variable', [1])
+ if ENV not in os.environ:
+    logging.info('%s is not an environment variable', ENV)
  else:
     print('Success')
-    logging.info('Logging envrionment variable %s', os.environ[1])
+    logging.info('Logging envrionment variable %s', os.environ[ENV])
  
 while True:
  main()

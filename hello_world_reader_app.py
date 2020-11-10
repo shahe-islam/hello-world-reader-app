d0da6ae22805535a199476c320b1a59e91c17636
Shahe Islam <shahe.islam@nationwide.co.uk>
2020-11-10 11:09:41 +0000

    changed logging destination

--- hello_world_reader_app.py
+++ hello_world_reader_app.py
@@ -2,11 +2,12 @@
 
 import os, time, sys, logging
 
+#References period of logging for ENV
 ENV = 'HELLOWORLD'
 TIMER = 3
  
 def main():
- logging.basicConfig(filename='/tmp/hello_world_reader_app.log', level=logging.INFO)
+ logging.basicConfig(filename='helloWorldReadApp.log', level=logging.INFO)
  
  if ENV not in os.environ:
     logging.info('%s is not an environment variable', ENV)
